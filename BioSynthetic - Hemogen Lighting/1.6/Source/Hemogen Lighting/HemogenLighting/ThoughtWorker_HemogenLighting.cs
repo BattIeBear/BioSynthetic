c83ff87021623339ada79dc996c014b760341549
BattIeBear <69400780+BattIeBear@users.noreply.github.com>
2025-06-26 18:41:07 -0400

    1.6 Upload

---
+++ BioSynthetic - Hemogen Lighting/1.6/Source/Hemogen Lighting/HemogenLighting/ThoughtWorker_HemogenLighting.cs
@@ -0,0 +1,45 @@
+﻿using RimWorld;
+using Verse.AI;
+using Verse;
+
+namespace BioSynth_HemogenLighting
+{
+    class ThoughtWorker_HemogenLighting : ThoughtWorker
+    {
+        private const float MaxDist = 8f;
+
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            if (!ModsConfig.BiotechActive)
+            {
+                return false;
+            }
+            if (p.Suspended)
+            {
+                return false;
+            }
+
+            Room pawnRoom = p.GetRoom();
+
+            if (GenClosest.ClosestThingReachable(p.Position, p.Map, ThingRequest.ForDef(ThingDefOf.biosynth_HemogenLamp), PathEndMode.ClosestTouch, TraverseParms.For(p), 8f, delegate (Thing b)
+            {
+                Room room = b.GetRoom();
+                return (room == null || room == pawnRoom) && (b.TryGetComp<CompGlower>()?.Glows ?? false);
+            }) == null
+            && GenClosest.ClosestThingReachable(p.Position, p.Map, ThingRequest.ForDef(ThingDefOf.biosynth_MacabreHemogenWallLamp), PathEndMode.ClosestTouch, TraverseParms.For(p), 8f, delegate (Thing b)
+            {
+                Room room = b.GetRoom();
+                return (room == null || room == pawnRoom) && (b.TryGetComp<CompGlower>()?.Glows ?? false);
+            }) == null
+            && GenClosest.ClosestThingReachable(p.Position, p.Map, ThingRequest.ForDef(ThingDefOf.biosynth_HemogenWallLamp), PathEndMode.ClosestTouch, TraverseParms.For(p), 8f, delegate (Thing b)
+            {
+                Room room = b.GetRoom();
+                return (room == null || room == pawnRoom) && (b.TryGetComp<CompGlower>()?.Glows ?? false);
+            }) == null)
+            {
+                return false;
+            }
+            return ThoughtState.ActiveAtStage(0);
+        }
+    }
+}
---
+++ BioSynthetic - Hemogen Lighting/1.6/Source/Hemogen Lighting/Things/Building_MacabreHemogenWallLamp.cs
@@ -0,0 +1,45 @@
+﻿using RimWorld;
+using Verse;
+
+namespace BioSynth_HemogenLighting
+{
+    [StaticConstructorOnStartup]
+    public class Building_MacabreHemogenWallLamp : Building
+    {
+        private CompFlickable flickableComp;
+
+        private CompPowerTrader powerTraderComp;
+
+        private Graphic offGraphic;
+        public override Graphic Graphic
+        {
+            get
+            {
+                if (flickableComp.SwitchIsOn && powerTraderComp.PowerOn)
+                {
+                    return DefaultGraphic;
+                }
+
+                if (offGraphic == null)
+                {                                   //Type graphicClass, Texture2D texture, Shader shader, Vector2 drawSize, Color color, Color colorTwo, GraphicData data, List<ShaderParameter> shaderParameters, string maskPath = null
+                    offGraphic = GraphicDatabase.Get(def.graphicData.graphicClass, def.graphicData.texPath + "_Off", def.graphicData.shaderType.Shader, def.graphicData.drawSize, DrawColor, DrawColorTwo, def.graphicData, def.graphicData.shaderParameters, def.graphic.maskPath);
+                }
+
+                return offGraphic;
+            }
+        }
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            flickableComp = GetComp<CompFlickable>();
+            powerTraderComp = GetComp<CompPowerTrader>();
+        }
+        public override void PostMake()
+        {
+            base.PostMake();
+            flickableComp = GetComp<CompFlickable>();
+            powerTraderComp = GetComp<CompPowerTrader>();
+        }
+    }
+}
