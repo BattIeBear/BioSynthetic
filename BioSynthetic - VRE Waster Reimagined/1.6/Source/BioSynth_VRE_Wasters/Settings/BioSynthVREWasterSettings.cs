02e1ab2d087b34428d5a38ba684d05917f659694
BattIeBear <69400780+BattIeBear@users.noreply.github.com>
2025-07-12 09:19:26 -0400

    1.6 Upload

---
+++ BioSynthetic - VRE Waster Reimagined/1.6/Source/BioSynth_VRE_Wasters/Settings/BioSynthVREWasterSettings.cs
@@ -0,0 +1,24 @@
+﻿using Verse;
+
+namespace BioSynth_VRE_Wasters
+{
+    public class BioSynthVREWasterSettings : ModSettings
+    {
+        ///cosmetic options
+        public static bool AddNewHairColors = true;
+        public static bool AddNewSkinColors = true;
+        public static bool DisableGauntBody = true;
+
+        ///gameplay options
+        public static bool KeepInstability = true;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref AddNewHairColors, "AddNewHairColors");
+            Scribe_Values.Look(ref AddNewSkinColors, "AddNewSkinColors");
+            Scribe_Values.Look(ref DisableGauntBody, "DisableGauntBody");
+            Scribe_Values.Look(ref KeepInstability, "KeepInstability");
+            base.ExposeData();
+        }
+    }
+}
---
+++ BioSynthetic - VRE Waster Reimagined/1.6/Source/BioSynth_VRE_Wasters/Settings/BioSynthVREWasterSettingsWindow.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace BioSynth_VRE_Wasters
+{
+    public class BioSynthVREWasterSettingsWindow : Mod
+    {
+        /// A reference to our settings.
+        BioSynthVREWasterSettings settings;
+
+        /// A mandatory constructor which resolves the reference to our settings.
+        public BioSynthVREWasterSettingsWindow(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<BioSynthVREWasterSettings>();
+        }
+
+        /// The (optional) GUI part to set your settings.
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.Label("BioSynth_VREWasters_TopWarning".Translate());
+
+            listingStandard.GapLine();
+
+            listingStandard.Label("BioSynth_VREWasters_Label1".Translate());
+            listingStandard.CheckboxLabeled("BioSynth_VREWasters_AddNewHairColors".Translate(), ref BioSynthVREWasterSettings.AddNewHairColors, "BioSynth_VREWasters_AddNewHairColorsDesc".Translate());
+            listingStandard.CheckboxLabeled("BioSynth_VREWasters_AddNewSkinColors".Translate(), ref BioSynthVREWasterSettings.AddNewSkinColors, "BioSynth_VREWasters_AddNewSkinColorsDesc".Translate());
+            listingStandard.CheckboxLabeled("BioSynth_VREWasters_DisableGauntBody".Translate(), ref BioSynthVREWasterSettings.DisableGauntBody, "BioSynth_VREWasters_DisableGauntBodyDesc".Translate());
+
+            listingStandard.GapLine();
+
+            listingStandard.Label("BioSynth_VREWasters_Label2".Translate());
+            listingStandard.CheckboxLabeled("BioSynth_VREWasters_KeepInstability".Translate(), ref BioSynthVREWasterSettings.KeepInstability, "BioSynth_VREWasters_KeepInstabilityDesc".Translate());
+
+            listingStandard.Label("BioSynth_VREWasters_BottomWarning".Translate());
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        /// Override SettingsCategory to show up in the list of settings.
+        /// Using .Translate() is optional, but does allow for localization.
+        public override string SettingsCategory()
+        {
+            return "BioSynth_VREWasters_ModName".Translate();
+        }
+    }
+}
